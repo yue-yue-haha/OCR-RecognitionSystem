99e85b4a542113fd6fa0e7eeaaac57a109ef6a99
shatanyumi <1143005768@qq.com>
2020-11-09 20:10:33 +0800

    DatabaseManager

--- DatabaseManager/DatabaseManager.py
+++ DatabaseManager/DatabaseManager.py
@@ -1,36 +1,7 @@
 # Database manager for controlling the database
-import sqlite3
 
 
 class DatabaseManager(object):
 
-    def __init__(self, db, jsonstr):
+    def __init__(self, db):
         self.db = db
-        self.jsonstr = jsonstr
-
-    def store(self):
-        con = sqlite3.connect(self.db)
-
-        cur = con.cursor()
-
-        cur.execute("create table if not exists datas(name,title,telephone,email,company,address)")
-
-        tmp = self.jsonstr
-        # print(tmp['name'][0], tmp['title'][0], tmp['tel'][0], tmp['email'][0],tmp['comp'][0],tmp['addr'][0])
-
-        cur.executemany("insert into datas(name,title,telephone,email,company,address) values (?,?,?,?,?,?)",
-                        [(tmp['name'][0], tmp['title'][0], tmp['tel'][0], tmp['email'][0], tmp['comp'][0],
-                          tmp['addr'][0])])
-
-        # for row in con.execute("select name,title,telephone,email,company,address from datas"):
-        #     print(row)
-
-        con.commit()
-        con.close()
-
-    def get_all(self):
-        con = sqlite3.connect(self.db)
-        tables = con.execute("select name,title,telephone,email,company,address from datas")
-
-        return tables
-
---
+++ ToolClasses/JsonDataDealer.py
@@ -0,0 +1,34 @@
+# Json Data Dealer to deal the json, and return the data
+import sqlite3
+
+
+class JsonDataDealer(object):
+    def __init__(self, db, jsonstr):
+        self.db = db
+        self.jsonstr = jsonstr
+
+    def store(self):
+        con = sqlite3.connect(self.db)
+
+        cur = con.cursor()
+
+        cur.execute("create table if not exists datas(name,title,telephone,email,company,address)")
+
+        tmp = self.jsonstr
+        # print(tmp['name'][0], tmp['title'][0], tmp['tel'][0], tmp['email'][0],tmp['comp'][0],tmp['addr'][0])
+
+        cur.executemany("insert into datas(name,title,telephone,email,company,address) values (?,?,?,?,?,?)",
+                        [(tmp['name'][0], tmp['title'][0], tmp['tel'][0], tmp['email'][0], tmp['comp'][0],
+                          tmp['addr'][0])])
+
+        # for row in con.execute("select name,title,telephone,email,company,address from datas"):
+        #     print(row)
+
+        con.commit()
+        con.close()
+
+    def get_all(self):
+        con = sqlite3.connect(self.db)
+        tables = con.execute("select name,title,telephone,email,company,address from datas")
+
+        return tables
